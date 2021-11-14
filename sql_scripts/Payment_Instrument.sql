4f53fc6db710be212222927c6499e4b825c11624
Bikal Lamichhane <blamichh@villanova.edu>
2021-11-14 10:20:42 +0000

    Added SQL Scripts for Data Entry

---
+++ sql_scripts/Payment_Instrument.sql
@@ -0,0 +1,8 @@
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (0, 'American Express' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (1, 'Discover' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (2, 'Visa' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (3, 'Mastercard' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (4, 'Paypal' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (5, 'Venmo' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (6, 'Cashapp' );
+Insert into Payment_Instrument (PAYMENT_TYPE_ID,PAYMENT_TYPE_NAME) values (7, 'Duo' );
