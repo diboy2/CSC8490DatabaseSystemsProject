b0f0ea730adc59d038fc40110b13ef22900c5c28
Adrian C <cagaananadrian@gmail.com>
2021-11-24 10:44:56 -0500

    Added stylesheets

---
+++ client/dormdash/src/pages/Whatever.js
@@ -0,0 +1,12 @@
+import './Whatever.scss';
+import * as React from "react";
+
+const Whatever = () => {
+  return (
+    <div className="whatever">
+      Whatev
+    </div>
+  )
+};
+
+export default Whatever
